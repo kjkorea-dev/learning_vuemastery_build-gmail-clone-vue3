ac4cb07ec37aa28c46ebef15d9f5c7451e2174ca
kj <agassi77@gmail.com>
2021-09-09 11:33:13 +0900

    Building the Inbox : end

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,32 @@
 <template>
   <h1>VMail Inbox</h1>
+  <table class="mail-table">
+    <tr
+      v-for="email in unarchivedEmails"
+      :key="email.id"
+      :class="['clickable', email.read ? 'read' : '']"
+      @click="email.read = true"
+    >
+      <td><input type="checkbox" /></td>
+      <td>
+        <p>
+          <strong>{{ email.subject }}</strong> - {{ email.body }}
+        </p>
+      </td>
+      <td class="date">{{ format(new Date(email.sentAt), 'MMM do yyyy') }}</td>
+      <td><button @click="email.archived = true">Archive</button></td>
+    </tr>
+  </table>
 </template>
 
 <script>
+import { format } from 'date-fns'
+
 export default {
   name: 'App',
   data() {
     return {
+      format,
       emails: [
         {
           id: 1,
@@ -49,6 +69,16 @@ export default {
         },
       ],
     }
+  },
+  computed: {
+    sortedEmails() {
+      return [...this.emails].sort((e1, e2) => {
+        return e1.sentAt < e2.sentAt ? 1 : -1
+      })
+    },
+    unarchivedEmails() {
+      return this.sortedEmails.filter((e) => !e.archived)
+    },
   },
 }
 </script>
